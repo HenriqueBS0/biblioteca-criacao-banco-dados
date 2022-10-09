4f645251414c559a27674040923a6715dbbeaf2e
Henrique Borges dos Santos <henrique.10agr@gamil.com>
2022-10-09 12:10:40 -0300

    Criada classe que faz conexao com o banco de dados

--- src/main/java/com/bd/Conexao.java
+++ src/main/java/com/bd/Conexao.java
@@ -2,31 +2,14 @@ package com.bd;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Conexao {
-    public static void main(String[] args) throws SQLException {
+    public static Connection get(String jdbcDriver, String jdbcUrl, String port, String database, String user, String password) throws ClassNotFoundException, SQLException {
+        Class.forName(jdbcDriver);
 
-        Connection conexao = null;
+        String urlConnetion = String.format("%s:%s/%s", jdbcUrl, port, database);
 
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");   
-            conexao = DriverManager.getConnection("jdbc:mysql://localhost/aviacao", "root", "123456");
-            ResultSet resultSet = conexao.createStatement().executeQuery("SELECT * FROM crew");
-
-            while (resultSet.next()) {
-                System.out.println("Nome: " + resultSet.getString("crew_job"));
-            }
-
-        } catch (ClassNotFoundException exception) {
-            System.out.println("Driver não localizado");
-        } catch (SQLException exception) {            
-            System.out.println("Erro ao acessar o banco: " + exception.getMessage());
-        } finally {
-            if(conexao != null) {
-                conexao.close();
-            }
-        }
+        return DriverManager.getConnection(urlConnetion, user, password);
     }
 }
